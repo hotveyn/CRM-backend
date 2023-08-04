a03316ef4c2c15e112caa505ff807caaefc79572
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-08-05 00:12:54 +0300

    update: app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,41 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { DepartmentModule } from './department/department.module';
+import { UserDepartmentsModule } from './join-tables/user-departments/user-departments.module';
+import { UtilsModule } from './utils/utils.module';
+import { BreakModule } from './break/break.module';
+import { OrderModule } from './order/order.module';
+import { OrderStageModule } from './order-stage/order-stage.module';
+import { ReclamationModule } from './reclamation/reclamation.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    UserModule,
+    AuthModule,
+    DepartmentModule,
+    UserDepartmentsModule,
+    UtilsModule,
+    BreakModule,
+    OrderModule,
+    OrderStageModule,
+    ReclamationModule,
+  ],
   controllers: [],
   providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true }));
+  await app.listen(PORT);
 }
 bootstrap();
