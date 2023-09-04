7d4cd055bafd9c743d11cc49a0b708c6109a8a02
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-09-04 05:08:18 +0300

    delete reclamations

--- src/reclamation/reclamation.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { ReclamationService } from './reclamation.service';
-
-@Controller('reclamation')
-export class ReclamationController {
-  constructor(private readonly reclamationService: ReclamationService) {}
-}
