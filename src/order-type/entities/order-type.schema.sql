ae2181e61a7199b3a2e3617ebbccf1d38bcaacab
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-11-23 12:52:34 +0300

    Create order-type.schema.sql

---
+++ src/order-type/entities/order-type.schema.sql
@@ -0,0 +1,7 @@
+create table order_types
+(
+    id serial primary key,
+    name varchar(255) not null
+    "createdAt"   timestamp with time zone not null,
+    "updatedAt"   timestamp with time zone not null
+);
