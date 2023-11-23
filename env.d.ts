fa6fae2f52e1590ed3a865284b432000a60e117c
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-11-23 13:58:04 +0300

    Create env.d.ts

---
+++ env.d.ts
@@ -0,0 +1,17 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      PORT: string;
+      JWT_SECRET_USER_AUTH: string;
+      DB_HOST: string;
+      DB_PORT: string;
+      DB_USERNAME: string;
+      DB_PASSWORD: string;
+      DB_NAME: string;
+    }
+  }
+}
+
+// If this file has no import/export statements (i.e. is a script)
+// convert it into a module by adding an empty export statement.
+export {};
