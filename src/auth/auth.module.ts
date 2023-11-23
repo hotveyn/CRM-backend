c4c693e42b94bb85c624198640efb025f2ffeab9
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-11-23 17:38:38 +0300

    provide jwt secret key through ENV

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,13 +3,13 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from './constants';
+
 //TODO: захуячить глобальным
 @Module({
   imports: [
     forwardRef(() => UserModule),
     JwtModule.register({
-      secret: jwtConstants.secret,
+      secret: process.env.JWT_SECRET_USER_AUTH,
       signOptions: { expiresIn: '720d' },
     }),
   ],
