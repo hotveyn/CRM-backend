10d2a46e4456803c1aaeb1f92ae6e7f6eaebc61f
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-07 23:08:38 +0300

    feat: create sql schema of new tables

---
+++ src/prefab/entities/prefab.schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE prefabs
+(
+    id        serial primary key,
+    name      varchar(255)                                         not null,
+    code      varchar(255),
+    comment   varchar(255),
+    price     real                                                 not null,
+    type_id   bigint references order_types (id) on delete cascade not null,
+    createdAt timestamp with time zone,
+    updatedAt timestamp with time zone
+);
