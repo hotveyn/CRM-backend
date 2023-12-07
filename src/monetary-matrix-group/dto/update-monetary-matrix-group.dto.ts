d6dac7c5cf298500d9cee2ef0a7fc06a98f3e171
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-07 21:36:37 +0300

    delete: useless

--- src/monetary-matrix-group/dto/update-monetary-matrix-group.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateMonetaryMatrixGroupDto } from './create-monetary-matrix-group.dto';
-
-export class UpdateMonetaryMatrixGroupDto extends PartialType(CreateMonetaryMatrixGroupDto) {}
--- src/monetary-matrix-group/monetary-matrix-group.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MonetaryMatrixGroupService } from './monetary-matrix-group.service';
-import { MonetaryMatrixGroupController } from './monetary-matrix-group.controller';
-
-@Module({
-  controllers: [MonetaryMatrixGroupController],
-  providers: [MonetaryMatrixGroupService]
-})
-export class MonetaryMatrixGroupModule {}
