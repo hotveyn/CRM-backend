8e1250c5d56b5854bbb13d797040c21659d43d8d
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-10 23:19:31 +0300

    add config init in module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import 'dotenv/config';
+import { ConfigModule } from '@nestjs/config';
 
 //TODO: захуячить глобальным
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
     forwardRef(() => UserModule),
     JwtModule.register({
       secret: process.env.JWT_SECRET_USER_AUTH,
