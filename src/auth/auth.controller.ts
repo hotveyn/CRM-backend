b4199e050db4e01891eeb305164ba3f91a6b4efd
Ivan Shestopalov <61887221+hotveyn@users.noreply.github.com>
2023-12-11 17:49:18 +0300

    rename endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,7 +12,7 @@ export class AuthController {
     return this.authService.login(loginDto);
   }
 
-  @Post('reg-employee')
+  @Post('reg')
   registerEmployee(@Body() regDto: RegDto) {
     return this.authService.regEmployee(regDto);
   }
